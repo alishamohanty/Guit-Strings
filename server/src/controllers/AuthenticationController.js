66fdefe24158b586ec3973a8f20cb1130d03b31d
Alisha Mohanty <alishamohanty1416@gmail.com>
2018-07-10 19:09:25 +0530

    Login code working (without authentication)

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -10,5 +10,33 @@ module.exports = {
           error:'This email account already exsists!!'
       })
     }   
+  },
+  async login(req, res){
+    try {
+      console.log('Inside login method')
+      const {email, password} = req.body;
+      const user = await User.findOne({
+        where:{
+        email: email
+      }
+    })
+    if(!user) {
+      return res.status(403).send({
+        error: 'The User is not registered!!'})
+    } 
+    const isPassword = password === user.password
+    if (!isPassword) {
+      return res.status(403).send({
+      error: 'The Password entered is invalid'})
+    }
+    const userJson = user.toJSON()
+    res.send({
+      user: userJson
+    })    
+    } catch (error) {
+      res.status(400).send({
+      error:'Some issue with login'
+    })
+    }
   }
 }
--- server/src/routes.js
+++ server/src/routes.js
@@ -5,4 +5,5 @@ module.exports = (app) => {
   app.post('/register',
     AuthenticationControllerPolicy.register,
     AuthenticationController.register)
+  app.post('/login', AuthenticationController.login)
 }
