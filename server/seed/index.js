0bd96e71ba1ed43b7f9a65ea292889f6906a9385
Alisha Mohanty <alishamohanty1416@gmail.com>
2018-07-19 12:07:40 +0530

    Bookmark functionality added

--- server/seed/index.js
+++ server/seed/index.js
@@ -1,11 +1,13 @@
 const {
   sequelize,
   Song,
-  User
+  User,
+  Bookmark
 } = require('../src/models')
 const Promise = require('bluebird')
 const songs = require('./songs.json')
 const users = require('./users.json')
+const bookmarks = require('./bookmarks.json')
 
 sequelize.sync({force: true})
   .then(async function () {
@@ -19,4 +21,9 @@ sequelize.sync({force: true})
         Song.create(song)
       })
     )
+    await Promise.all(
+        bookmarks.map(bookmark => {
+          Bookmark.create(bookmark)
+        })
+      )
   })
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -19,6 +19,11 @@ fs
     const model = sequelize.import(path.join(__dirname, file))
     db[model.name] = model
   })
+Object.keys(db).forEach(function (modelName) {
+  if ('associate' in db[modelName]) {
+    db[modelName].associate(db)
+  }
+})
 db.sequelize = sequelize
 db.Sequelize = Sequelize
 module.exports = db
